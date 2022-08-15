c01e3adfb28bdd28bebd23873707ebad1545c430
Vince <53957319+vincengai@users.noreply.github.com>
2022-08-15 15:31:33 -0700

    fixed jobs section

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,32 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
 // ...GatsbyImageSharpFluid
+import axios from 'axios';
 
 const ProjectsPage = () => {
+  const [projectsData, setprojectsData] = useState({});
 
-  const res =  fetch("/projects/")
+  const qs = require('qs');
+  const query = qs.stringify({
+    populate: '*', 
+  }, {
+    encodeValuesOnly: true, // prettify URL
+  });
+
+  useEffect( () => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+      axios.get(`http://localhost:1337/api/projects?${query}`).then(response => {
+        setprojectsData(response?.data); 
+      })
+    };
+
+  console.log(projectsData, 'hola')  
+
   return (
    <div>
      <h2>projects page</h2>
